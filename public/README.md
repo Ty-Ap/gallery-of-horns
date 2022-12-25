c1e3866d0d1f9554b815f98a77ac6dc3a4bb786a
Ty Aponte <aaustinsk8@aol.com>
2022-12-25 11:30:41 -0800

    added favorites functionality

--- public/README.md
+++ public/README.md
@@ -3,8 +3,29 @@ Estimate of time needed to complete: 30min
 
 Start time: 430
 
+Finish time: 6 
+
+Actual time needed to complete:90mins ish. brute forced because i forgot about .map
+
+Number and name of feature: Favorite
+
+Estimate of time needed to complete: 30 mins
+
+Start time: 615
+
+Finish time: 630
+
+Actual time needed to complete: 15 minutes
+
+
+Number and name of feature: Bootstrap
+
+Estimate of time needed to complete: _____
+
+Start time: _____
+
 Finish time: _____
 
-Actual time needed to complete: ___
+Actual time needed to complete: _____
 
 
--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -1,13 +1,29 @@
 import React from 'react';
 
+
 // HornedBeast component needs to contain an <h2> that displays the title of the animal, an <img> element with src, alt and title attributes, and a <p> that displays the description.
 
 class HornedBeast extends React.Component{
+  constructor(props){
+    super(props);
+    this.state={
+      favorites :0,
+    }
+  }
+
+  handleFavorite = () => {
+    this.setState({
+      favorites : this.state.favorites+1
+    })
+  }
+
   render(){
     return <>
     <h2>{this.props.title}</h2>
-    <img src={this.props.image_url} alt={this.props.alt} title={this.props.title}></img>
+    <img id ={this.props._id} src={this.props.image_url} alt={this.props.alt}></img>
     <p>{this.props.description}</p>
+    <p onClick={this.handleFavorite}>favorite?</p>
+    <p> favorited 💚{this.state.favorites} </p>
     
     </>
 
